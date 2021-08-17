792f320441163d551b5dbf45519fcfb9d27c6db4
William Alexander Aguirre Antolínez <86641219+AguirreAntolinez@users.noreply.github.com>
2021-08-16 22:37:13 -0500

    Update README.R

--- README.R
+++ README.R
@@ -1,4 +1,4 @@
-# muestreoaleatoriosimple
+# Muestreo aleatorio simple
 #Código en R para calcular tamaño de muestra y extraerla de un data frame usando muestreo aleatorio simple.  
 n<-(qnorm(1-0.05/2)^2*nrow(df)*0.5^2)/
   (0.05^2*(nrow(df)-1)
